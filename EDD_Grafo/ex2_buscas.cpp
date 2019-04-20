6e22da049dd5418bceb8e79394058191917094fd
Joao Fellipe <felippe.uller@gmail.com>
2019-04-20 17:31:58 -0300

    Implementação Busca em Largura (EX2) finalizada

--- EDD_Grafo/ex2_buscas.cpp
+++ EDD_Grafo/ex2_buscas.cpp
@@ -1,4 +1,5 @@
 #include "include/SimpleGraph.h"
+#include <queue>
 
 using namespace std;
 
@@ -12,22 +13,80 @@ int main(int argc, char const *argv[]) {
         return 1;
     }
 
-	// verificar se o construtor conseguiu criar o grafo e senao retornar erro
+
+    // criacao do grafo e leitura do arquivo de entrada para populacao
+    GrafoSimples* grafo = new GrafoSimples();
+
+    if (!grafo->ler(argv[1])) {
+        cout << "Arquivo inexistente\n";
+    }
 
 
-	// criacao do grafo e populacao do mesmo utilizando o metodo de leitura de arquivo (argv[1])
-	GrafoSimples* grafo;
-	int vertice_origem = stoi(argv[2]);
+	// definicao do vertice de origem da busca
+	int s = stoi(argv[2]);
+    int num_vertices = grafo->qtdVertices();
 
 	// verifica se o vertice passado por parametro existe dentro do grafo
-	if (vertice_origem > grafo->qtdVertices()) {
-		cout << "O vértice " << vertice_origem << " não existe no grafo " << argv[1] << endl;
+	if (s > num_vertices) {
+		cout << "O vértice " << s << " não existe no grafo " << argv[1] << endl;
 		return 1;
 	}
 
 
-	// busca em largura
-	
+	// BUSCA EM LARGURA (nao utiliza Av por nao precisar armazenar os antecessores)
+    // Inicializacao
+    bool* c = new bool[num_vertices];
+    int* d = new int[num_vertices];
+
+    for (int i = 0; i < num_vertices; i++) {
+        c[i] = false;
+        d[i] = INFINITE_INT;
+    }
+
+    c[s] = true;
+    d[s] = 0;
+
+    queue<int> q = *new queue<int>();
+    q.push(s);
+
+
+    // Propagacao das visitas
+    while(!q.empty()) {
+        int u = q.front();
+        q.pop();
+
+        for (int v : grafo->vizinhos(u)) {
+            if (!c[v]) {
+                c[v] = true;
+                d[v] = d[u] + 1;
+                q.push(v);
+            }
+        }
+    }
+
+
+    // Saida
+    int nivel = 0;
+    bool control = true;
+    while (control) {
+        control = false;
+        string out = to_string(nivel) + ": ";
+
+        for (int i = 0; i < num_vertices; i++) {
+            if (d[i] == nivel) {
+                control = true;
+                out += to_string(i) + ",";
+            }
+        }
+
+        if (control) {
+            out.pop_back();
+            cout << out << endl;
+            nivel++;
+        }
+    }
+
+    // na hora de imprimir lembrar de utilizar vertice+1
 
 
 	return 0;
