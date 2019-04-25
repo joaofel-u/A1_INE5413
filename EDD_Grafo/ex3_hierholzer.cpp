c684df59414207a218248ce21310c69a1977e4dc
Joao Fellipe <felippe.uller@gmail.com>
2019-04-25 13:31:29 -0300

    Implementação Hierholzer para ciclos Eulerianos terminada

--- EDD_Grafo/ex3_hierholzer.cpp
+++ EDD_Grafo/ex3_hierholzer.cpp
@@ -1,10 +1,10 @@
 #include "include/SimpleGraph.h"
 #include <algorithm>
-#include <vector> 
+#include <list>
 
 using namespace std;
 
-vector<int> buscarSubcicloEuleriano(GrafoSimples* g, int v, int** c);
+bool buscarSubcicloEuleriano(GrafoSimples* g, int v, int** c, list<int>* ciclo);
 
 int main(int argc, char const *argv[]) {
 
@@ -29,11 +29,15 @@ int main(int argc, char const *argv[]) {
 
 	// CICLO EULERIANO (algoritmo de Hierholzer)
     // Inicializacao
-    int** c = new int*[num_vertices];
+    int** c = new int*[num_vertices+1];
 
+    // Inicializacao
+    for (int i = 0; i <= num_vertices; i++)
+        c[i] = new int[num_vertices+1];
+
+    // populacao
     for (int i = 1; i <= num_vertices; i++) {
-        c[i] = new int[num_vertices];
-        for(int j = i; j < num_vertices; j++) {
+        for(int j = i; j <= num_vertices; j++) {
             if (grafo->haAresta(i, j)) {
                 c[i][j] = 0;
                 c[j][i] = 0;
@@ -42,34 +46,33 @@ int main(int argc, char const *argv[]) {
                 c[j][i] = -1;
             }
         }
-        
+
     }
 
-    int v = 0;
-    bool r = false;
+    int v = 1;
 
-    vector<int> ciclo;
-    ciclo = buscarSubcicloEuleriano(grafo, v, c);
+    list<int>* ciclo = new list<int>();
+    if(buscarSubcicloEuleriano(grafo, v, c, ciclo)) {
+        string caminho;
+        auto final = ciclo->end()--;
+        auto it = ciclo->begin();
 
-    // saida
-    // testar se n tem nd no ciclo dai retorna false '0'
-    string caminho;
-    int tamanhoCiclo;
-    for(int i = 0; i < tamanhoCiclo; i++) {
-        caminho += ciclo[i];
+        for(; it != final; it++)
+            caminho += to_string(*it) + ",";
+
+        caminho += to_string(*it);
+
+        cout << "1\n" << caminho << endl;
+    } else {
+        cout << "0\n";
     }
-    
-
 
 	return 0;
 }
 
-vector<int> buscarSubcicloEuleriano(GrafoSimples* g, int v, int** c) {
-    int iteredorCiclo = -1;
-    vector<int> ciclo = *new vector<int>();
-    ciclo.push_back(v);
+bool buscarSubcicloEuleriano(GrafoSimples* g, int v, int** c, list<int>* ciclo) {
+    ciclo->push_back(v);
     int t = v;
-
     do {
         int u = -1;
         for (int viz : g->vizinhos(v)) {
@@ -78,9 +81,10 @@ vector<int> buscarSubcicloEuleriano(GrafoSimples* g, int v, int** c) {
                 break;
             }
         }
+
         // Só prossegue se existir uma aresta não-visitada conectada a ciclo.
         if (u == -1) {
-            return *new vector<int>();
+            return false;
         } else {
             // {v,u} ← selecionar uma aresta e ∈ E tal que Ce=false
             // Caso entre no else, a aresta que satisfaz a condicao
@@ -89,22 +93,27 @@ vector<int> buscarSubcicloEuleriano(GrafoSimples* g, int v, int** c) {
             c[v][u] = 1;
             v = u;
             // Adiciona o vértice v ao final do ciclo.
-            ciclo.push_back(v);
+            ciclo->push_back(v);
         }
     } while (v != t);
 
-    //ver q diabos significa aquele ultimo foreach
-    for(int i = 0; i < ciclo.size(); i++) {
-        int w = ciclo[i];
+
+    // verifica se existem subciclos independentes dentro do ciclo maior
+    for(auto it = ciclo->begin(); it != ciclo->end(); it++) {
+        int w = *it;
+
         for (int viz : g->vizinhos(w)) {
             if (!c[viz][w]) {
-                
+                list<int>* subciclo = new list<int>();
+
+                if (buscarSubcicloEuleriano(g, v, c, subciclo)) {
+                    for(auto sub_it = (subciclo->begin())++; sub_it != subciclo->end(); sub_it++)
+                        it = ciclo->insert(it, *sub_it);
+                }
             }
         }
     }
-    
 
-    //ciclo
-    return;
+
+    return true;
 }
-
